ddb297e2a108833fbb4fe88336b42320a8a0953c
Afifudin Maarif <32411703+afifudinmtop@users.noreply.github.com>
2024-01-15 15:28:58 +0700

    v.1.0.45
    manajemen-produk-app-mern-nextjs-docker

---
+++ manajemen-produk-app-mern-nextjs-docker/frontend/app/produk/[id]/page.js
@@ -0,0 +1,103 @@
+"use client";
+import { useState, useEffect } from "react";
+
+const produk = ({ params }) => {
+  const id = params.id;
+  const [name, setName] = useState("");
+  const [qty, setQty] = useState(0);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = () => {
+    fetch("https://tes3.apip.dev//products/" + id, {
+      cache: "no-store",
+      method: "GET",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setName(data.name);
+        setQty(data.qty);
+      })
+      .catch((error) => {
+        console.error("Error fetching data: ", error);
+      });
+  };
+
+  const updateData = () => {
+    fetch("https://tes3.apip.dev//products/" + id, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ name, qty }),
+    })
+      .then(() => {
+        window.location.href = "/";
+      })
+      .catch((error) => {
+        console.error("Error fetching data: ", error);
+      });
+  };
+
+  const deleteData = () => {
+    fetch("https://tes3.apip.dev//products/" + id, {
+      method: "delete",
+    })
+      .then(() => {
+        window.location.href = "/";
+      })
+      .catch((error) => {
+        console.error("Error fetching data: ", error);
+      });
+  };
+
+  return (
+    <div className="w-50 mx-auto bg-white p-4 rounded mt-4">
+      <div className="mb-3 d-flex gap-3">
+        <label className="form-label my-auto label_form">Name</label>
+        <input
+          type="text"
+          className="form-control"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </div>
+
+      <div className="mb-3 d-flex gap-3">
+        <label className="form-label my-auto label_form">Qty</label>
+        <input
+          type="number"
+          className="form-control"
+          value={qty}
+          onChange={(e) => setQty(e.target.value)}
+        />
+      </div>
+
+      <div className="d-flex mt-4">
+        <div className="col-6 d-grid p-3">
+          <button
+            type="button"
+            onClick={updateData}
+            className="btn btn-success fw-bold "
+          >
+            Simpan
+          </button>
+        </div>
+
+        <div className="col-6 d-grid p-3">
+          <button
+            type="button"
+            onClick={deleteData}
+            className="btn btn-danger fw-bold "
+          >
+            Hapus
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default produk;
---
+++ manajemen-produk-app-mern-nextjs-docker/server/app.js
@@ -0,0 +1,84 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const app = express();
+
+app.use(express.json());
+const url_mongo = "mongodb://mongo:27017/productdb";
+
+app.use(cors());
+
+const productSchema = new mongoose.Schema({
+  name: String,
+  qty: Number,
+});
+
+const Product = mongoose.model("Product", productSchema);
+
+// Endpoint to list all products
+app.get("/products", async (req, res) => {
+  const products = await Product.find();
+  res.json(products);
+});
+
+// Endpoint to create a new product
+app.post("/products", async (req, res) => {
+  const product = new Product(req.body);
+  await product.save();
+  res.status(201).json(product);
+});
+
+// Endpoint to get a single product by ID
+app.get("/products/:id", async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+    if (!product) {
+      return res.status(404).send("Product not found");
+    }
+    res.json(product);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+});
+
+// Endpoint to update a product by ID
+app.put("/products/:id", async (req, res) => {
+  try {
+    const product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    if (!product) {
+      return res.status(404).send("Product not found");
+    }
+    res.json(product);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+});
+
+// Endpoint to delete a product by ID
+app.delete("/products/:id", async (req, res) => {
+  try {
+    const product = await Product.findByIdAndDelete(req.params.id);
+    if (!product) {
+      return res.status(404).send("Product not found");
+    }
+    res.send("Product deleted");
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+});
+
+// MongoDB connection
+mongoose
+  .connect(url_mongo, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log(err));
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
